0ea9378b9690f55014d3b724f332906e77e63eca
celestialxevermore <key2317@naver.com>
2022-08-07 23:03:36 +0900

    20220807 정기연습

---
+++ PS2022_cpp/PS_cpp/PS_cpp/욕심쟁이 판다.cpp
@@ -0,0 +1,68 @@
+// 메모리 : 23632 KB 
+// 시간 : 132 ms
+
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#define MAX 501
+using namespace std;
+
+int N;
+int map[MAX][MAX];
+bool visited[MAX][MAX];
+int dp[MAX][MAX];
+int maximum = -1;
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1,0,0 };
+
+
+int DFS(int a, int b) {
+	int dis = 0;
+
+	if (dp[a][b] == -1) {
+		dp[a][b] = 1;
+		for (int i = 0; i < 4; i++) {
+
+			int x = a + dx[i];
+			int y = b + dy[i];
+
+			if (y >= 1 && x >= 1 && y <= N && x <= N && map[a][b] < map[x][y]) {
+				dis = max(dis, DFS(x, y));
+			}
+
+
+		}
+		dp[a][b] += dis;
+	}
+
+	return dp[a][b];
+
+
+}
+
+
+int main() {
+	int ans = 0;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cin >> map[i][j];
+			dp[i][j] = -1;
+		}
+	}
+
+
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+
+			ans = max(ans, DFS(i, j));
+
+
+		}
+	}
+	printf("%d\n", ans);
+
+
+}
--- PS2022_cpp/PS_cpp/PS_cpp/이동하기.cpp
+++ PS2022_cpp/PS_cpp/PS_cpp/이동하기.cpp
@@ -1,34 +1,34 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
-#include<queue>
-#define MAX 1001
-using namespace std;
-
-int N, M;
-
-int map[MAX][MAX];
-int dp[MAX][MAX];
-
-int main() {
-
-	cin >> N >> M;
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= M; j++) {
-			cin >> map[i][j];
-		}
-	}
-
-	for (int i = 2; i <= N; i++) {
-		dp[i][1] = dp[i - 1][1] + map[i][1];
-		dp[1][i] = dp[1][i - 1] + map[1][i];
-	}
-
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= M; j++) {
-			dp[i][j] = map[i][j] + max(dp[i - 1][j], dp[i][j - 1]);
-		}
-	}
-	cout << dp[N][M];
-}
-
+//#include<iostream>
+//#include<algorithm>
+//#include<vector>
+//#include<queue>
+//#define MAX 1001
+//using namespace std;
+//
+//int N, M;
+//
+//int map[MAX][MAX];
+//int dp[MAX][MAX];
+//
+//int main() {
+//
+//	cin >> N >> M;
+//	for (int i = 1; i <= N; i++) {
+//		for (int j = 1; j <= M; j++) {
+//			cin >> map[i][j];
+//		}
+//	}
+//
+//	for (int i = 2; i <= N; i++) {
+//		dp[i][1] = dp[i - 1][1] + map[i][1];
+//		dp[1][i] = dp[1][i - 1] + map[1][i];
+//	}
+//
+//	for (int i = 1; i <= N; i++) {
+//		for (int j = 1; j <= M; j++) {
+//			dp[i][j] = map[i][j] + max(dp[i - 1][j], dp[i][j - 1]);
+//		}
+//	}
+//	cout << dp[N][M];
+//}
+//
